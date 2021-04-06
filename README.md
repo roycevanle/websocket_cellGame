0f996d88dddce3f8cdfb8e9dc9522042d2cfa52a
roycevanle <roycele123@gmail.com>
2021-04-05 22:24:16 -0700

    implemented join and create websocket connection methods as well as the client-side join & create dispaly

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # websocket_cellGame
+
+# Learned Lessons
+1. Be careful how you craft your connection.on stuff. Mispellings and an extra () => ruined things for sure. Failed silently so it was hard to identify where it failed at first.
+
--- index.js
+++ index.js
@@ -10,6 +10,7 @@ httpServer.listen(9090, () => console.log("Listening on 9090"));
 
 //hasmap
 const clients = {};
+const games = {};
 
 const wsServer = new websocketServer({
     "httpServer": httpServer
@@ -20,18 +21,65 @@ wsServer.on("request", request => {
     const connection = request.accept(null, request.origin); //gets a beautiful tcp connection, biDirectional thing
     connection.on("open", () => console.log("opened!"))
     connection.on("close", () => console.log("closed!"))
-    connection.on("message", () => message => {
+    connection.on("message", message => {
         const result = JSON.parse(message.utf8Data)
+        
         //i have receieved a message from the client
-        console.log(result)
+        //a client wants to create a new game
+        if (result.method === "create") {
+            console.log("made it here");
+            const clientId = result.clientId;
+            const gameId = guid();
+            games[gameId] = {
+                "id": gameId,
+                "balls": 20,
+                "clients": []
+            }
+
+            const payLoad = {
+                "method": "create",
+                "game": games[gameId]
+            }
+
+            const con = clients[clientId].connection;
+            con.send(JSON.stringify(payLoad));
+
+        }
+
+        //a client wants to join
+        if (result.method === "join") {
+            const clientId = result.clientId;
+            const gameId = result.gameId;
+            const game = games[gameId];
+            if (game.clients.length >= 3) {
+                //sorry max players reached
+                return;
+            }
+            const color = {"0": "Red", "1": "Green", "2": "Blue"}[game.clients.length]
+            game.clients.push({
+                "clientId": clientId,
+                "color": color
+            })
+
+            const payLoad = {
+                "method": "join",
+                "game": game
+            }
+
+            game.clients.forEach(c => {
+                clients[c.clientId].connection.send(JSON.stringify(payLoad));
+            })
+        }
+
     })
 
-    // generate a new clientId
+    // generate a new clientId value with key being the accepted connection object
     const clientId = guid();
     clients[clientId] = {
         "connection": connection
     }
 
+    // payload to send client so they know their clientId for future requests
     const payLoad = {
         "method": "connect",
         "clientId": clientId
